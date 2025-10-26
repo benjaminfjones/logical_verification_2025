83f0508ceb37079f614f3f20fbd4670f4634b698
Benjamin Jones <benjaminfjones@gmail.com>
2025-10-26 23:35:26 +0000

    Add LoVe06 exercises

--- lean/LoVe/LoVe06_InductivePredicates_ExerciseSheet.lean
+++ lean/LoVe/LoVe06_InductivePredicates_ExerciseSheet.lean
@@ -20,6 +20,7 @@ namespace LoVe
 The `Even` predicate is `True` for even numbers and `False` for odd numbers. -/
 
 #check Even
+#print Even
 
 /- We define `Odd` as the negation of `Even`: -/
 
@@ -32,20 +33,35 @@ Hint: `cases` or `induction` is useful to reason about hypotheses of the form
 `Even …`. -/
 
 @[simp] theorem Odd_1 :
-    Odd 1 :=
-  sorry
+    Odd 1 := by
+  intro h
+  cases h
 
 /- 1.2. Prove that 3 and 5 are odd. -/
 
--- enter your answer here
+theorem Odd_3 : Odd 3 := by
+  intro h
+  cases h
+  simp at a
+  cases a
+
+theorem Odd_5 : Odd 5 := by
+  intro h
+  cases h
+  simp at a
+  cases a
+  cases a_1
+  -- grind -- fails
 
 /- 1.3. Complete the following proof by structural induction. -/
 
 theorem Even_two_times :
     ∀m : ℕ, Even (2 * m)
   | 0     => Even.zero
-  | m + 1 =>
-    sorry
+  | m + 1 => by
+    rw [mul_add, mul_one]
+    apply Even.add_two
+    exact Even_two_times m
 
 
 /- ## Question 2: Tennis Games
@@ -57,35 +73,39 @@ Recall the inductive type of tennis scores from the demo: -/
 /- 2.1. Define an inductive predicate that returns `True` if the server is
 ahead of the receiver and that returns `False` otherwise. -/
 
-inductive ServAhead : Score → Prop
-  -- enter the missing cases here
+inductive ServAhead : Score → Prop where
+  | vs_ahead : ∀ (n m: Nat), n > m → ServAhead (Score.vs n m)
+  | adv : ServAhead Score.advServ
+  -- | not_disAdv : ¬ (ServAhead Score.advRecv)  -- isn't an application of `ServAhead`
+  | game : ServAhead Score.gameServ
 
 /- 2.2. Validate your predicate definition by proving the following theorems. -/
 
 theorem ServAhead_vs {m n : ℕ} (hgt : m > n) :
     ServAhead (Score.vs m n) :=
-  sorry
+  ServAhead.vs_ahead _ _ hgt
 
 theorem ServAhead_advServ :
-    ServAhead Score.advServ :=
-  sorry
+    ServAhead Score.advServ := ServAhead.adv
 
 theorem not_ServAhead_advRecv :
-    ¬ ServAhead Score.advRecv :=
-  sorry
+    ¬ ServAhead Score.advRecv := by
+  intro h
+  cases h
 
 theorem ServAhead_gameServ :
-    ServAhead Score.gameServ :=
-  sorry
+    ServAhead Score.gameServ := ServAhead.game
 
 theorem not_ServAhead_gameRecv :
-    ¬ ServAhead Score.gameRecv :=
-  sorry
+    ¬ ServAhead Score.gameRecv := by
+  intro h
+  cases h
 
 /- 2.3. Compare the above theorem statements with your definition. What do you
 observe? -/
 
--- enter your answer here
+-- The positive theorems correspond to constructors of the inductive Prop and
+-- the negative theorems correspond to omissions of constructors.
 
 
 /- ## Question 3: Binary Trees
@@ -97,24 +117,65 @@ theorems (e.g., `IsFull_mirror`, `mirror_mirror`). -/
 #check mirror_mirror
 
 theorem mirror_IsFull {α : Type} :
-    ∀t : Tree α, IsFull (mirror t) → IsFull t :=
-  sorry
+    ∀t : Tree α, IsFull (mirror t) → IsFull t := by
+  intro t
+  intro hfm
+  rw [← mirror_mirror t]
+  apply IsFull_mirror
+  assumption
 
 /- 3.2. Define a `map` function on binary trees, similar to `List.map`. -/
 
-def Tree.map {α β : Type} (f : α → β) : Tree α → Tree β :=
-  sorry
+/- NOTE: Tree.map conflicts with a def in mathlib which is compatible with
+   our Tree, but simp uses the mathlib version which is very confusing
+   below. -/
+def treeMap {α β : Type} (f : α → β) : Tree α → Tree β
+  | .nil => .nil
+  | .node a l r => .node (f a) (treeMap f l) (treeMap f r)
 
 /- 3.3. Prove the following theorem by case distinction. -/
 
 theorem Tree.map_eq_empty_iff {α β : Type} (f : α → β) :
-    ∀t : Tree α, Tree.map f t = Tree.nil ↔ t = Tree.nil :=
-  sorry
+    ∀t : Tree α, treeMap f t = Tree.nil ↔ t = Tree.nil := by
+  intro t
+  cases t
+  { simp only [treeMap] }
+  { constructor <;>
+    /- both directions are ruled out by cases -/
+    { intro h
+      cases h } }
 
 /- 3.4 (**optional**). Prove the following theorem by rule induction. -/
 
-theorem map_mirror {α β : Type} (f : α → β) :
-    ∀t : Tree α, IsFull t → IsFull (Tree.map f t) :=
-  sorry
+theorem Tree.IsFull_map {α β : Type} (f : α → β) :
+    ∀t : Tree α, IsFull t → IsFull (treeMap f t) := by
+  intro t
+  induction t with
+  | nil => intro _; simp only [treeMap]; exact IsFull.nil
+  | node a l r hl hr =>
+    intro h
+    simp only [treeMap]
+    cases h
+    have hl' : IsFull (treeMap f l) := hl hl_1
+    have hr' : IsFull (treeMap f r) := hr hr_1
+    apply IsFull.node
+    . exact hl'
+    . exact hr'
+    . constructor
+      { intro hfl
+        have : l = Tree.nil := by
+          apply (Tree.map_eq_empty_iff f l).mp
+          assumption
+        apply (Tree.map_eq_empty_iff f r).mpr
+        exact (hiff.mp this)
+      }
+      -- same but l |-> r and .mp |-> .mpr
+      { intro hfl
+        have : r = Tree.nil := by
+          apply (Tree.map_eq_empty_iff f r).mp
+          assumption
+        apply (Tree.map_eq_empty_iff f l).mpr
+        exact (hiff.mpr this)
+      }
 
 end LoVe
